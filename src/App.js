9a9022c7ef54331651c0964c44b22c0fb2d58e70
Ali Berenjian <ali.rberenjian@gmail.com>
2019-05-12 23:39:17 -0700

    Setup Pinged

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import PingComponent from "./ping_component/ping_component";
 import "./App.css";
 
--- src/ping_component/ping_component.js
+++ src/ping_component/ping_component.js
@@ -1,9 +1,9 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+// import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import CircularProgress from "@material-ui/core/CircularProgress";
@@ -23,8 +23,8 @@ const styles = theme => ({
   wrapper: {
     width: "30%"
   },
-  summary_container:{
-    "& div:first-child":{
+  summary_container: {
+    "& div:first-child": {
       justifyContent: "space-between !important"
     }
   }
@@ -37,78 +37,93 @@ class PingComponent extends React.Component {
       IntervalID: null,
       count: 0,
       servers: [
-       {
-        name: "Skype",
-        ready: false,
-        detail: null,
-        response_times: [],
-        average: null,
-        url: "https://www.skype.com/en/"
-      },
-      {
-        name: "GoToMeeting",
-        ready: false,
-        detail: null,
-        response_times: [],
-        average: null,
-        url: "https://www.gotomeeting.com/"
-      },
-      // {
-      //   name: "Service Now",
-      //   ready: false,
-      //   detail: null,
-      //   response_times: [],
-      //   average: null,
-      //   url: "https://www.servicenow.com/"
-      // },
-      // {
-      //   name: "Sales Force",
-      //   ready: false,
-      //   detail: null,
-      //   response_times: [],
-      //   average: null,
-      //   url: "https://www.salesforce.com/"
-      // },
-      {
-        name: "Slack",
-        ready: false,
-        detail: null,
-        response_times: [],
-        average: null,
-        url: "https://slack.com/"
-      }
-    ]
-    }
-    this.preping = this.preping.bind(this)
-    this.PingHost = this.PingHost.bind(this)
-  }
-
-  componentDidMount(){
-    this.preping()
-  }
-
-  async preping(){
-    let new_servers = []
-    // for(let i = 0 ; i < this.state.servers.length;i++){
-    //   console.log("Pinged ", this.state.servers[i].name)
-    //   let server = await this.PingHost(this.state.servers[i],i)
-    // }
-    for (const server of this.state.servers){
-      let s = await this.PingHost(server)
-    }
-    console.log("Done")
-  }
-
-  async PingHost(server,) {
-      let p = new Ping()
-      p.ping(server.url,(err,data)=>{
-        if (err){
-          
-        }else{
-          console.log(data)
-          server.response_times.push(data)
+        {
+          name: "Skype",
+          ready: false,
+          detail: null,
+          response_times: [],
+          average: null,
+          url: "https://www.skype.com/en/"
+        },
+        {
+          name: "GoToMeeting",
+          ready: false,
+          detail: null,
+          response_times: [],
+          average: null,
+          url: "https://www.gotomeeting.com/"
+        },
+        // {
+        //   name: "Service Now",
+        //   ready: false,
+        //   detail: null,
+        //   response_times: [],
+        //   average: null,
+        //   url: "https://www.servicenow.com/"
+        // },
+        // {
+        //   name: "Sales Force",
+        //   ready: false,
+        //   detail: null,
+        //   response_times: [],
+        //   average: null,
+        //   url: "https://www.salesforce.com/"
+        // },
+        {
+          name: "Slack",
+          ready: false,
+          detail: null,
+          response_times: [],
+          average: null,
+          url: "https://slack.com/"
         }
-      })
+      ]
+    };
+    this.preping = this.preping.bind(this);
+    this.PingHost = this.PingHost.bind(this);
+  }
+
+  componentDidMount() {
+    this.preping();
+  }
+
+  async preping() {
+    let new_servers = [];
+    for (let i = 0; i < this.state.servers.length; i++) {
+      let server = this.state.servers[i];
+      let results = await this.PingHost(this.state.servers[i]);
+      server.response_times = [...results];
+      server.ready = true;
+      let sum = results.reduce((acc, curr) => acc + curr);
+      server.average = (sum / results.length).toFixed(2);
+      new_servers.push(server);
+    }
+    this.setState({
+      servers: new_servers
+    });
+    console.log("Done");
+  }
+
+  calculateAverage(results) {}
+
+  PingHost(server) {
+    console.log(server.name);
+    let results = [];
+    let IntervalID = setInterval(() => {
+      let p = new Ping();
+      p.ping(server.url, (err, data) => {
+        if (err) {
+        } else {
+          results.push(data);
+        }
+      });
+    }, 100);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        clearInterval(IntervalID);
+        resolve(results);
+      }, 1000);
+    });
   }
 
   render() {
@@ -116,22 +131,25 @@ class PingComponent extends React.Component {
     return (
       <div className={classes.root}>
         <div className={classes.wrapper}>
-         {this.state.servers.map((server,key)=>(
+          {this.state.servers.map((server, key) => (
             <ExpansionPanel key={key}>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} className={classes.summary_container}>
-              <Typography className={classes.heading}>{server.name}</Typography>
-              {!server.ready ? (
-                <div>
-                  <CircularProgress size={25}/>
-                </div>
-              ):(
-                <Typography>
-                  {server.average}
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                className={classes.summary_container}
+              >
+                <Typography className={classes.heading}>
+                  {server.name}
                 </Typography>
-              )}
-            </ExpansionPanelSummary>
-          </ExpansionPanel>
-         ))}
+                {!server.ready ? (
+                  <div>
+                    <CircularProgress size={25} />
+                  </div>
+                ) : (
+                  <Typography>{server.average}</Typography>
+                )}
+              </ExpansionPanelSummary>
+            </ExpansionPanel>
+          ))}
         </div>
       </div>
     );
